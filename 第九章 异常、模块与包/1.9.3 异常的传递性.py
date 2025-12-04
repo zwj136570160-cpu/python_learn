fa9832a5af01b790ea89a05faec5178c89fe9f17
ZWJ136570160-CPU <zwj136570160@gmail.com>
2025-12-04 19:44:56 +0800

    Commit Message

--- 第九章 异常、模块与包/1.9.3 异常的传递性.py
+++ 第九章 异常、模块与包/1.9.3 异常的传递性.py
@@ -11,11 +11,11 @@ def func02():   # 异常在func02中没有被捕获
     print("这是func02开始")
     func01()
     print("这是func02结束")
-# def main():   # 异常在main中被捕获
-#     try:
-#         func02()
-#     except Exception as e:
-#         print(e)
+def main():   # 异常在main中被捕获
+    try:
+        func02()
+    except Exception as e:
+        print(e)
 """
 - 异常是具有传递性的
 当函数func01中发生异常，并且没有捕获处理这个异常的时候，异常会传递刀函数func02，当func02也没有捕获处理这个异常的时候，main函数会捕获这个异常，这就是异常的传递性
